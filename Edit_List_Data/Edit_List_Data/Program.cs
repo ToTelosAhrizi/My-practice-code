f71eeace0cab7d1530aceaddce8b539fef787ab7
ToTelosAhrizi <harperdoug.j@gmail.com>
2018-03-01 16:59:12 -0500

    Working on a program to edit text files. WIP

--- Edit_List_Data/Edit_List_Data/Program.cs
+++ Edit_List_Data/Edit_List_Data/Program.cs
@@ -1,38 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Console;
-
-namespace Changing_List_Data
-{/// <summary>
+﻿using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Text; 
+using System.Threading.Tasks; 
+using static System.Console; 
+ 
+namespace Changing_List_Data 
+{/// <summary> 
 /// This program will allow the user to input data into a list. Once input they will be allowed to edit the list by deleting one item, change the value of one
-/// item, or adding one item to a specific point in the list.
-/// </summary>
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            List<int> practice = new List<int>();//first a new list is created and named practice
+/// item, or adding one item to a specific point in the list. 
+/// </summary> 
+    class Program 
+    { 
+        static void Main(string[] args) 
+        { 
+            List<int> practice = new List<int>();//first a new list is created and named practice 
             practice.Add(1);//list start at zero, so as to not to confuse the user the start point of the list is set to zero
-            int start = 1;//All list must have at least one entry so we are setting this
-            int x = 2;//x will be the variable being added to the list. It gets its first value here
-            WriteLine("How many entries?(Enter a valid (whole, positive) number. Non Valid entries will close.)");
+            int start = 1;//All list must have at least one entry so we are setting this 
+            int x = 2;//x will be the variable being added to the list. It gets its first value here 
+            WriteLine("How many entries?(Enter a valid (whole, positive) number. Non Valid entries will close.)"); 
             if (int.TryParse(ReadLine(), out int entries)&&entries>0)// this tells the progam what to do if the user enters a valid number of how many items to add to the list.
-            {
+            { 
                 /*will add x to the list, raise the value of x and raise the value of start. Will loop this action 
-                 * until start is equal to the number of entries the user input*/
+                 *until start is equal to the number of entries the user input*/
                 while (start != entries)
-                {
+                { 
                     practice.Add(x);
-                    x++;
-                    start++;
-                }
-                
-                string p = string.Join("\n", practice.ToArray());//adds list to an array and converts the list to a human readable string for display
-                WriteLine(p);//displaying the values of the practice array
-                WriteLine("\nWhat entry would you like to change");//ask what item in the list they want to change
+                    x++; 
+                    start++; 
+                }                
+                string p = string.Join("\n", practice.ToArray());//adds list to an array and converts the list to a human readable string for display 
+                WriteLine(p);//displaying the values of the practice array 
+                WriteLine("\nWhat entry would you like to change");//ask what item in the list they want to change 
                 if (int.TryParse(ReadLine(), out int change)&& practice.Contains(change))//reads what item in the list the user wants to change
                 {
                     WriteLine("\nPress 1 to delete, 2 to update, 3 to add an entry. All else will close.");//ask the user what they want to do to that item in the list
@@ -83,14 +82,13 @@ namespace Changing_List_Data
                 {
                     WriteLine("GoodBye!");
                     ReadKey();
-                }
-                
-            }
-            else// what to do if the user doesnt provide a valid entry for the number of entries in the list
-            {
-                WriteLine("GoodBye!");
-                ReadKey();
-            }
-        }
-    }
-}
+                }                
+            } 
+            else// what to do if the user doesnt provide a valid entry for the number of entries in the list 
+            { 
+                WriteLine("GoodBye!"); 
+                ReadKey(); 
+            } 
+        } 
+    }  
+} 
---
+++ ScratchPad4/ScratchPad4/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ScratchPad4
+{
+    class Program
+    {
+        public static int n = 1;
+        static void Main(string[] args)
+        {
+            string again = "yes";
+            while (again !="no" && again !="No" && again != "NO" && again != "nO")
+            {
+                WriteLine("Type 1 to creat a new note or 2 to read a note");
+                string choice = ReadLine();
+                if (choice == "1")
+                {
+                    CreateInfo();
+                    WriteLine("Again?");
+                    again = ReadLine();
+                }
+                else if (choice == "2")
+                {
+                    ReadInfo();
+                    WriteLine("Again?");
+                    again = ReadLine();
+                }
+                else if (choice == "3")
+                {
+                    EditInfo();
+                    again = ReadLine();
+                }
+                else
+                {
+                    WriteLine("Non valid entry, again?");
+                    again = ReadLine();
+                }
+            }
+            
+        }
+
+        public static void CreateInfo()
+        {
+            string note = "";
+            
+            List<string> notes = new List<string>();
+            WriteLine("Write your notes, type XXX to finish");
+            while (note != "XXX")
+            {
+
+                int index = (n);
+                Write(n + ". ");
+                note = ReadLine();
+                if (note != "XXX")
+                {
+                    notes.Add(note);
+                    n++;
+                }
+                else
+                {
+                    WriteLine("Ok, all done!\n");
+                }
+
+            }
+            WriteLine("What do you want to name these notes?");
+            string notename = ReadLine();
+            string StNotes = string.Join("\n", notes.ToArray());
+            string[] noteList = { StNotes };
+            System.IO.File.WriteAllLines(@"C:\Users\harperd7\Desktop\Notes on " + notename + ".doc", noteList);
+            System.IO.File.WriteAllLines(@"C:\Users\harperd7\Desktop\Notes on " + notename + ".csv", noteList);
+
+        }
+        public static void ReadInfo()
+        {
+            WriteLine("What's the name of the file?");
+            string notename = ReadLine();
+            string checkingdoc = @"C:\Users\harperd7\Desktop\Notes on " + notename + ".doc";
+            string checkingcsv = @"C:\Users\harperd7\Desktop\Notes on " + notename + ".csv";
+            if (File.Exists(checkingdoc))
+                if (File.Exists(checkingcsv))
+                {
+                Write(System.IO.File.ReadAllText(@"C:\Users\harperd7\Desktop\Notes on " + notename + ".doc"));
+                }
+                
+            ReadKey();
+        }
+        public static void EditInfo()
+        {
+            List<string> notes = new List<string>();
+            WriteLine("What's the name of the file?\n");
+            string notename = ReadLine();
+            
+            if (File.Exists(@"C:\Users\harperd7\Desktop\Notes on " + notename + ".doc"))
+            {
+                string [] mylist = System.IO.File.ReadAllLines(@"C:\Users\harperd7\Desktop\Notes on " + notename + ".doc");                
+                string listed = string.Join(n+"\n", mylist.ToArray());
+                Write("\n" + listed + "\n");
+                notes.AddRange(mylist);
+                WriteLine("\nWhat entry would you like to change");
+                int.TryParse(ReadLine(), out int line);
+                WriteLine("1 to delete the line, 2 to add a change after that line, 3 change the line itself");
+                int.TryParse(ReadLine(), out int change);
+                if (change == 1)
+                {
+                    notes.RemoveAt(line - 1);
+                    string nlist = string.Join("\n", notes.ToArray());
+                    Write("\n" + nlist);
+                    string StNotes = string.Join("\n", notes.ToArray());
+                    string[] noteList = { StNotes };
+                    System.IO.File.WriteAllLines(@"C:\Users\harperd7\Desktop\Notes on " + notename + ".doc", noteList);
+                    ReadKey();
+                }
+                else if (change == 2)
+                {
+                    WriteLine("What would you like to add?");
+                    notes.Insert(line, ReadLine());
+                    string nlist = string.Join("\n", notes.ToArray());
+                    Write("\n" + nlist);
+                    string StNotes = string.Join("\n", notes.ToArray());
+                    string[] noteList = { StNotes };
+                    System.IO.File.WriteAllLines(@"C:\Users\harperd7\Desktop\Notes on " + notename + ".doc", noteList);
+                    ReadKey();
+                }
+                else if (change == 3)
+                {
+                    WriteLine("What would you like to change it to?");
+                    notes.Insert(line, ReadLine());
+                    notes.RemoveAt(line - 1);
+                    string nlist = string.Join("\n", notes.ToArray());
+                    Write("\n" + nlist);
+                    string StNotes = string.Join("\n", notes.ToArray());
+                    string[] noteList = { StNotes };
+                    System.IO.File.WriteAllLines(@"C:\Users\harperd7\Desktop\Notes on " + notename + ".doc", noteList);
+                    ReadKey();
+                }
+                else
+                {
+                    WriteLine("Non Valid");
+                }
+
+
+
+            }
+            else
+            {
+                WriteLine("Does not exist");
+            }
+           
+
+
+        }
+    }
+}
