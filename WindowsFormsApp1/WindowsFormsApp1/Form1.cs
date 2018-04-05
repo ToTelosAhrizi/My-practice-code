84b93af0e741e52fff16e4b59d07e5fb6da661d0
ToTelosAhrizi <harperdoug.j@gmail.com>
2018-04-05 14:38:48 -0400

    Changed Some code location

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.NetworkInformation;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string maca = string.Empty;
+            NetworkInterface[] lookfornic = NetworkInterface.GetAllNetworkInterfaces();
+            foreach(NetworkInterface nic in lookfornic)
+            {
+                if (nic.OperationalStatus == OperationalStatus.Up)
+                {
+                    maca += nic.GetPhysicalAddress().ToString();
+                }
+                
+            }
+            
+            List<string> validkeys = new List<string>(File.ReadAllLines(@"C:/Users/Jay/Documents/keys.txt"));
+           if (!(validkeys.Contains(maca)))
+            {
+                int entry = 0;
+                while (!(validkeys.Contains(maca)))
+                {
+
+                    if (validkeys.ElementAt(entry).Count() != 12)
+                    {
+                        validkeys.Insert(entry, maca);
+                    }
+                    else
+                    {
+                        entry++;
+                    }
+                }
+                File.WriteAllLines(@"C:/Users/Jay/Documents/keys.txt", validkeys);
+                richTextBox1.Text = "You're good to go!";
+
+            }
+            else
+            {
+                richTextBox1.Text = "You're great!";
+            }
+            
+        }
+
+        private void fileSystemWatcher1_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            if (File.Exists("C:/Users/Jay/Documents/keys.txt"))
+            {
+                Application.Run(new Form1());
+            }
+            else
+            {
+                Application.Run(new Form2());
+                Process.Start("C:/Users/Jay/Documents/GitHub/My-practice-code/Generate Key/Generate Key/bin/Debug/Generate Key.exe");
+            }
+            
+        }
+    }
+}
